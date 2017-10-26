2450369f7dd0b5ccb45a29d810617969ad6858bd
Ivan Bravo <IvanBravo1@users.noreply.github.com>
2017-10-26 09:14:02 -0300

    codigo css

--- app/core/forms.py
+++ app/core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from app.core.models import OfertaLaboral
 
 class RegistroDesocupado(UserCreationForm):
     dni = forms.CharField(required=True)
